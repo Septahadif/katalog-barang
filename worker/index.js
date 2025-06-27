c754ac0bcd23690967f16f5c0dc9b984a359a947
Septahadif <110327728+Septahadif@users.noreply.github.com>
2025-06-27 10:09:31 +0800

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -925,7 +925,7 @@ class BarangApp {
 
   addNewItemToView(item) {
     const itemElement = this.createItemElement(item, 0);
-    this.katalog.append(itemElement);
+    this.katalog.insertBefore(itemElement, this.katalog.firstChild);
     itemElement.scrollIntoView({ behavior: 'smooth' });
   }
 
